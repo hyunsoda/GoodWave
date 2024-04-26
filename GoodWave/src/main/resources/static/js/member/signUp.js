e848a354fb41f93ab7996a99fa8474e2adb504d5
hyunsoda <so0144@naver.com>
2024-04-26 17:31:59 +0900

    sohyun

--- GoodWave/src/main/resources/static/js/member/signUp.js
+++ GoodWave/src/main/resources/static/js/member/signUp.js
@@ -27,3 +27,58 @@ function execDaumPostcode() {
 
 // 주소 검색 버튼 클릭 시
 document.querySelector("#searchAddress").addEventListener("click", execDaumPostcode);
+
+
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click",()=> {
+
+})
+
+
+
+
+
+
+
+const checkAll = document.querySelector('.checkbox_all .all');
+const checkBoxesNormal = document.querySelectorAll('.checkbox .normal');
+const checkBoxChecked = Array.from(checkBoxesNormal, (checkbox) => checkbox);
+function paintCheckAll(y) {
+    checkBoxChecked.map((e) => (e.checked = y));
+}
+function onCheckAllClick() {
+    checkAll.checked ? paintCheckAll(true) : paintCheckAll(false);
+}
+function onBoxClick() {
+    let is_checked = true;
+    checkBoxChecked.forEach((box) => {
+        is_checked = box.checked && is_checked;
+    });
+    checkAll.checked = is_checked;
+}
+checkAll.addEventListener('click', onCheckAllClick);
+checkBoxesNormal.forEach((checkbox) => checkbox.addEventListener('click', onBoxClick));
+
+
+
+$('.checkbox_all .all').click(function () {
+    if ($(this).prop('checked')) {
+        $('.checkbox .normal').prop('checked', true);
+    } else {
+        $('.checkbox .normal').prop('checked', false);
+    }
+});
+$('.checkbox').on('click', '.normal', function () {
+    var is_checked = true;
+    $('.checkbox .normal').each(function () {
+        is_checked = is_checked && $(this).is(':checked');
+    });
+    $('.checkbox_all .all').prop('checked', is_checked);
+});
+
+
+
+
+
+
