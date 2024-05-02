9be3027b45d68a6bc6511603e1b853d30bab0630
ana80202 <ana80202@gmail.com>
2024-05-02 17:42:00 +0900

    dayeon

--- GoodWave/src/main/resources/static/js/mypage/mypage.js
+++ GoodWave/src/main/resources/static/js/mypage/mypage.js
@@ -1,27 +1,31 @@
 function resetClass(element, classname){
-    element.classList.remove(classname);
-  }
-  document.getElementsByClassName("show-signup")[0].addEventListener("click",function(){
-    let form = document.getElementsByClassName("mypage")[0];
-    resetClass(form, "signin");
-    resetClass(form, "reset");
-    form.classList.add("signup");
-    document.getElementById("submit-btn").innerText = "취소하기";
-  });
-  
-  document.getElementsByClassName("show-signin")[0].addEventListener("click",function(){
-    let form = document.getElementsByClassName("mypage")[0];
-    resetClass(form, "signup");
-    resetClass(form, "reset");
-    form.classList.add("signin");
+  element.classList.remove(classname);
+}
 
+document.getElementsByClassName("show-info")[0].addEventListener("click",function(){
+  let form = document.getElementsByClassName("mypage")[0];
+  resetClass(form, "dlist");
+  resetClass(form, "alist");
+  form.classList.add("info");
 
-  });
-  document.getElementsByClassName("show-reset")[0].addEventListener("click",function(){
-    let form = document.getElementsByClassName("mypage")[0];
-    resetClass(form, "signup");
-    resetClass(form, "signin");
-    form.classList.add("reset");
-    document.getElementById("submit-btn").innerText = "취소하기";
-    
-  });
+  document.getElementsByClassName("mypage-elements").innerText = "hi";
+});
+
+document.getElementsByClassName("show-dlist")[0].addEventListener("click",function(){
+  let form = document.getElementsByClassName("mypage")[0];
+  resetClass(form, "info");
+  resetClass(form, "alist");
+  form.classList.add("dlist");
+
+  document.getElementsByClassName("donate-list").innerText = "hi";
+
+});
+document.getElementsByClassName("show-alist")[0].addEventListener("click",function(){
+  let form = document.getElementsByClassName("mypage")[0];
+  resetClass(form, "info");
+  resetClass(form, "dlist");
+  form.classList.add("alist");
+
+  document.getElementsByClassName("donate-list").innerText = "hi";
+
+});
